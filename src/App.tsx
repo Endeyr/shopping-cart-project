94c166a10767732331314dc5495f1097e00a361b
Endeyr <aaron.thompson42594@gmail.com>
2024-04-11 12:10:07 -0400

    add useEffect for fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import Layout from "./components/layout";
+import { fetchNameById, fetchPriceById } from "./services/api";
 
 function App() {
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      const priceObj = await fetchPriceById("4151", isMounted);
+      console.log(priceObj);
+      const name = await fetchNameById("4151", true);
+      console.log(name);
+    };
+    fetchData();
+    return () => {
+      isMounted = false; // mark component as unmounted
+    };
+  }, []);
+
   return (
     <>
       <Layout>
