b43bbf25a0682e4700f41f7e4248f528b02fdc66
Endeyr <aaron.thompson42594@gmail.com>
2024-04-16 10:00:01 -0400

    add vitest-fetch-mock

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,9 +1,16 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
+import { afterEach, expect, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
 
 expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
 });
+
+// vitest-fetch-mock
+const fetchMocker = createFetchMock(vi);
+
+// sets globalThis.fetch and globalThis.fetchMock to our mocked version
+fetchMocker.enableMocks();
