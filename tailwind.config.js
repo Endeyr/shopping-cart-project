2d329d0a4628f6dd1fef500ecfac9ca014ef3d2d
Endeyr <aaron.thompson42594@gmail.com>
2024-04-22 14:04:07 -0400

    added new style for osrs theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,38 +19,10 @@ module.exports = {
     },
     extend: {
       theme: {
-        "os_headings": ['PT Serif'],
-        "os_body": ['IBM Plex Sans'],
+        os_headings: ["PT Serif"],
+        os_body: ["IBM Plex Sans"],
       },
       colors: {
-        // osrs color theme
-        os_body_border: "#94866d",
-        os_body_dark: "#b8a282",
-        os_body_mid: "#d0bd97",
-        os_body_light: "#d8ccb4",
-        os_body_main: "#e2dbc8",
-        os_body_background: "#c0a886",
-        os_button_border: "#3c352a",
-        os_button_dark: "#18140c",
-        os_button_light: "#3a301d",
-        os_rsw_brown: "#605443",
-        os_link_color: "#936039",
-        os_background_link_color: "#52351e",
-        os_gray_mineshaft: "#333333",
-        os_gray_tundora: "#4c4c4c",
-        os_gray_boulder: "#777777",
-        os_gray_silver: "#cccccc",
-        os_gray_gallery: "#eeeeee",
-        os_gray_alabaster: "#f9f9f9",
-        // osrs status
-        os_error: "#801c13",
-        os_orange: "#7a3f08",
-        os_yellow: "#786300",
-        os_success: "#2e5e05",
-        os_blue: "#03436b",
-        // osrs dark theme
-        os_black: "#071022",
-        os_brown: "#1b1612",
         // shadcn color theme
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
