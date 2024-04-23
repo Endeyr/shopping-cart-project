0e61a9bb6c857ebbba77641bf0e9344ca863e60c
Endeyr <aaron.thompson42594@gmail.com>
2024-04-23 13:57:19 -0400

    add services component outline

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -1,6 +1,18 @@
 import Container from "@/components/container";
+import { ContentCard } from "../ui/content-card";
 
 const Services = () => {
-  return <Container>Services</Container>;
+  return (
+    <Container className="flex flex-col w-full justify-evenly items-center">
+      <h2 className="font-bold outline-black capitalize text-4xl text-center w-full">
+        Services
+      </h2>
+      <div className="flex flex-col xl:flex-row w-full justify-center items-center gap-4">
+        <ContentCard title="title">service</ContentCard>
+        <ContentCard title="title">service</ContentCard>
+        <ContentCard title="title">service</ContentCard>
+      </div>
+    </Container>
+  );
 };
 export default Services;
