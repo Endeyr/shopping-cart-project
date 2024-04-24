5bb482df48ea353f7c78bc20d14aab65319ae9b4
Endeyr <aaron.thompson42594@gmail.com>
2024-04-24 10:10:41 -0400

    add function to scroll to top of each page after load

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,11 @@
 import Footer from "../footer";
 import Navbar from "../navbar";
+import { ScrollToTop } from "../scroll-to-top";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative min-h-[100dvh]">
+      <ScrollToTop />
       <Navbar />
       <main>
         <div className="mx-auto">{children}</div>
