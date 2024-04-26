3b8dfb9da046e2e33fc070688d81c5d40263e04d
Endeyr <aaron.thompson42594@gmail.com>
2024-04-26 15:56:36 -0400

    add id to each link

--- src/components/navbar/nav-links/index.tsx
+++ src/components/navbar/nav-links/index.tsx
@@ -5,28 +5,34 @@ import { Link } from "react-router-dom";
 const NavLinks = () => {
   const links = [
     {
+      id: 1,
       url: "/about",
       name: "About",
     },
     {
+      id: 2,
       url: "/contact",
       name: "Contact",
     },
     {
+      id: 3,
       url: "/wish-list",
       name: "Wish List",
     },
     {
+      id: 4,
       url: "/cart",
       name: <GiShoppingCart size={30} />,
     },
   ];
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex items-center justify-center">
       {links.map((link) => (
-        <Link to={link.url} className={buttonVariants({ variant: "link" })}>
-          {link.name}
-        </Link>
+        <li key={link.id}>
+          <Link to={link.url} className={buttonVariants({ variant: "link" })}>
+            {link.name}
+          </Link>
+        </li>
       ))}
     </div>
   );
