057f39ebb0f7669a4489e4d23edc02e75f909af6
Endeyr <aaron.thompson42594@gmail.com>
2024-04-26 16:35:52 -0400

    updated buttonVariants import

--- src/components/navbar/nav-links/index.tsx
+++ src/components/navbar/nav-links/index.tsx
@@ -1,4 +1,4 @@
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button-variants";
 import { GiShoppingCart } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
