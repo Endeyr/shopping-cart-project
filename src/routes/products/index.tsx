6ac86962fdd89cdbe25370288e100eabade05a9b
Endeyr <aaron.thompson42594@gmail.com>
2024-04-29 09:03:31 -0400

    initial products page

--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -1,6 +1,29 @@
 import Container from "@/components/container";
 
 const ProductsPage = () => {
-  return <Container>Products</Container>;
+  return (
+    <Container className="grid grid-cols-5">
+      {/* Sidebar */}
+      <div className="w-full col-span-1">Sidebar</div>
+      {/* Main */}
+      <div className="flex flex-col w-full col-span-4 gap-2">
+        <h2>Products</h2>
+        <div className="grid w-full grid-cols-4 col-span-4">
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+          <div className="border h-[200px]">Item</div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 export default ProductsPage;
