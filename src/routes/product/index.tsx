4f46d3ee6cd72d95c80de1f8eb2a652074677feb
Endeyr <aaron.thompson42594@gmail.com>
2024-04-29 09:07:04 -0400

    add basic product page

--- src/routes/product/index.tsx
+++ src/routes/product/index.tsx
@@ -1,5 +1,17 @@
 import Container from "@/components/container";
 const ProductPage = () => {
-  return <Container>Product</Container>;
+  return (
+    <Container className="flex-col">
+      {/* Item */}
+      <h2>Item Name</h2>
+      <div className="grid w-full grid-cols-3">
+        <div className="h-[400px] border">Image</div>
+        <div className="h-[400px] border">Description</div>
+        <div className="h-[400px] border">Price + Add to Cart Buttons</div>
+      </div>
+      {/* Reviews / Comments */}
+      <div>Comments / Reviews</div>
+    </Container>
+  );
 };
 export default ProductPage;
