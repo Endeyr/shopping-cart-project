a16505a2cdf6ee96eb4cfcdfcc94052f7e2c4265
Endeyr <aaron.thompson42594@gmail.com>
2024-05-07 09:32:39 -0400

    basic confirmation layout

--- src/routes/confirmation/index.tsx
+++ src/routes/confirmation/index.tsx
@@ -1,6 +1,12 @@
 import Container from "@/components/container";
 
 const ConfirmationPage = () => {
-  return <Container>Confirmation Page</Container>;
+  return (
+    <Container className="flex-col">
+      <div>Order has shipped</div>
+      <div>Estimated Delivery Date</div>
+      <div>Back to Browsing Button</div>
+    </Container>
+  );
 };
 export default ConfirmationPage;
