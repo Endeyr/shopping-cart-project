17a4e3494900c291182c118a172de1acff23bf51
Endeyr <aaron.thompson42594@gmail.com>
2024-05-09 11:29:12 -0400

    add content to cards

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -8,9 +8,26 @@ const Services = () => {
         Services
       </h2>
       <div className="flex flex-col items-center justify-center w-full gap-4 xl:flex-row">
-        <ContentCard title="title">service</ContentCard>
-        <ContentCard title="title">service</ContentCard>
-        <ContentCard title="title">service</ContentCard>
+        <ContentCard title="Trade and Exchange">
+          As the premier marketplace in Al Kharid, Chancellor Hassan&apos;s
+          Bazaar serves as a hub for buying, selling, and trading various goods.
+          Adventurers can find everything from weaponry and armor to rare
+          artifacts and magical items, catering to adventurers of all levels.
+        </ContentCard>
+        <ContentCard title="Item Appraisal">
+          Skilled appraisers are available at the bazaar to assess the value of
+          treasures and artifacts brought in by adventurers. Whether it&apos;s
+          an ancient relic uncovered from a forgotten tomb or a unique item
+          obtained from a challenging quest, Chancellor Hassan&apos;s team
+          provides expert evaluation and fair prices.
+        </ContentCard>
+        <ContentCard title="Special Orders">
+          Chancellor Hassan&apos;s Bazaar welcomes special orders and
+          commissions from adventurers seeking custom-made equipment or unique
+          items tailored to their specific needs. Craftsmen and artisans in the
+          bazaar&apos;s employ can bring adventurers&apos; visions to life,
+          providing personalized gear and artifacts.
+        </ContentCard>
       </div>
     </Container>
   );
