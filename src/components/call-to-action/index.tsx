805f5f3c478cc5ebe589418a062993408af5112a
Endeyr <aaron.thompson42594@gmail.com>
2024-05-09 11:46:52 -0400

    add content to call to action component

--- src/components/call-to-action/index.tsx
+++ src/components/call-to-action/index.tsx
@@ -1,14 +1,41 @@
+import callToActionImg from "@/assets/images/items.webp";
 import Container from "@/components/container";
 import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+
 const CallToAction = () => {
   return (
-    <Container className="flex-col">
+    <Container className="flex-col py-6 bg-os_header_lite dark:bg-os_header_dark">
       <h2 className="w-full text-4xl font-bold text-center capitalize outline-black">
-        Buy from us
+        Experience the Selection for Yourself!
       </h2>
-      <p>paragraphs</p>
-      <p>Call to action here either a button or form</p>
-      <Link to={"/products/1"}>Products</Link>
+      <div className="grid w-full grid-cols-2 gap-16">
+        <div className="flex flex-col items-center justify-center w-full h-full gap-4 ml-4">
+          <p>
+            Ready to embark on your next adventure or stock up on essential
+            supplies? Look no further than Chancellor Hassan&apos;s Bazaar!
+            Visit us in Al Kharid to explore our vast selection of items,
+            ranging from everyday essentials to rare treasures.
+          </p>
+          <p>
+            Our knowledgeable staff is here to assist you every step of the way.
+            Come experience the magic of our bazaar for yourself and discover
+            why adventurers from all corners of Gielinor choose us as their
+            preferred trading hub. Your next great find awaits!
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full">
+          <img
+            src={callToActionImg}
+            alt="items"
+            className="size-[400px] hidden sm:block"
+            loading="lazy"
+          />
+        </div>
+      </div>
+      <Button>
+        <Link to={"/products/1"}>Check Out Our Products</Link>
+      </Button>
     </Container>
   );
 };
