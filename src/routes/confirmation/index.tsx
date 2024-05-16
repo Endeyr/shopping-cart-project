7cfbadb70b727ca24e1578e2cb6bb662d3d7d8c8
Endeyr <aaron.thompson42594@gmail.com>
2024-05-16 08:28:03 -0400

    styled page

--- src/routes/confirmation/index.tsx
+++ src/routes/confirmation/index.tsx
@@ -1,12 +1,23 @@
+import confirmBg from "@/assets/images/osrs.webp";
 import Container from "@/components/container";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const ConfirmationPage = () => {
-  // TODO style page
   return (
-    <Container className="flex-col">
-      <div>Order has shipped</div>
-      <div>Estimated Delivery Date</div>
-      <div>Back to Browsing Button</div>
+    <Container
+      className="py-12 mt-0 bg-center bg-no-repeat bg-cover shadow-lg xl:min-h-[calc(100dvh-80px)]"
+      style={{ backgroundImage: "url(" + confirmBg + ")" }}
+    >
+      <div
+        className="flex flex-col items-center justify-center w-full h-full gap-4 font-bold text-white"
+        style={{ textShadow: "1px 1px 2px black" }}
+      >
+        <div className="text-xl text-white">Thank you for your order!</div>
+        <Button>
+          <Link to={"/products/1"}>Back to Products</Link>
+        </Button>
+      </div>
     </Container>
   );
 };
